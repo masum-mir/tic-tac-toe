5956c664acb2a66dfd911cf70e45160af4a64b37
masum <masummir.lm@gmail.com>
2023-12-08 01:28:24 +0600

    Update game code

--- main.cpp
+++ main.cpp
@@ -1,65 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<graphics.h>
+#include <graphics.h>
+#include "conio.h"
 
-void board(int a[4][4]);
-
+void drawBoard(char *a[4][4]);
+void board(int x, int y);
 int main()
 {
-
-    int gd = DETECT, gm, x = 210, y = 150, i, j;
-
+    int gd = DETECT, gm, x = 210, y = 150, i=0, j;
 
     char *a[4][4] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"};
 
-
-    //char *a[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-
+    char firstPlayer[20], secondPlayer[20];
     initgraph(&gd, &gm, "");
-    setcolor(WHITE);
+    setcolor(GREEN);
 
-    for(i=0; i<4; i++)
+    // Wait for Enter key press
+    outtextxy(150, 100, "Welcome to our Tic Tac Toe");
+    outtextxy(170, 140, "Please click enter button.");
+    while(getch() != 13);
+    cleardevice();
+
+//   while (getch() != 13); // 13 is the ASCII code for Enter
+
+    outtextxy(220, 100, "Start Game");
+    outtextxy(220, 120, "Control");
+    outtextxy(220, 140, "Exits");
+
+    while(1)
     {
-        for (j=0; j<4; j++)
+        if(kbhit())
         {
-            setfillstyle(SOLID_FILL, RED);
+            int keyb = getch();
+            printf("Key::: %d", keyb);
+            if(keyb == 13)
+            {
+                while(1)
+                {
+                    if(kbhit())
+                    {
+                        char ascii = getch();
+                        printf("Second while loop: %d\n", ascii);
+                        if(ascii == 13)
+                        {
+                            //  firstPlayer[i]='\0';
+                            break;
+                        }
+                        else
+                        {
+                            //  for(ascii = 0; ascii <= 126; ascii++) {
+                            firstPlayer[i++] = ascii;
+                            cleardevice();
+                            outtextxy(100, 80, "First Player Name: ");
+                            //   char playerName[2] = {ascii, '\0'};
+                            outtextxy(300, 80, &ascii);
+                            //  }
 
-            rectangle(x,y,x+70,y+70);
-            //settextstyle(0,0,2);
-            // outtextxy(150, 100, a[i][j]);
-         //   outtextxy(x + 15, y + 15, a[i][j]);
-            // sprintf(*a, "%c", *a[i]);
+                        }
 
-            //  floodfill(x+1, y+1, BLACK);
-            x += 70;
-            // delay(30);
+                    }
+                }
+                cleardevice();
+                outtextxy(100, 50, "Name: ");
+                outtextxy(150, 50, firstPlayer);
+                board(x, y);
+
+            }
+            else if(keyb == 99 || keyb == 68)
+            {
+                cleardevice();
+                outtextxy(200, 100, "How to Play.");
+            }
+            else if(keyb == 27)
+            {
+                exit(0);
+            }
+
         }
-        //outtextxy(150, 10, a[i][j]);
-        x = 210;
-        y = y+70;
-        //  delay(30);
     }
 
-
-    //  board(a);
     getch();
+    closegraph();
     return 0;
 }
 
-void board(int a[4][4])
+void board(int x, int y)
 {
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            setfillstyle(SOLID_FILL, RED);
+            rectangle(x, y, x + 70, y + 70);
+            //outtextxy(x + 15, y + 15, a[i][j]);
+            x += 70;
+        }
+        x = 210;
+        y = y + 70;
+    }
+}
 
+void drawBoard(char *a[4][4])
+{
     printf("__________________________\n");
-    printf("|%d   | %d    | %d    | %d   |\n", a[0][0], a[0][1], a[0][2], a[0][3]);
+    printf("|%s   | %s    | %s    | %s   |\n", a[0][0], a[0][1], a[0][2], a[0][3]);
     printf("|    |      |      |     |\n");
     printf("|____|______|______|_____|\n");
-    printf("|%d   | %d    | %d    | %d   |\n", a[1][0], a[1][1], a[1][2], a[1][3]);
+    printf("|%s   | %s    | %s    | %s   |\n", a[1][0], a[1][1], a[1][2], a[1][3]);
     printf("|    |      |      |     |\n");
     printf("|____|______|______|_____|\n");
-    printf("|%d   | %d   | %d   | %d  |\n", a[2][0], a[2][1], a[2][2], a[2][3]);
+    printf("|%s   | %s   | %s   | %s  |\n", a[2][0], a[2][1], a[2][2], a[2][3]);
     printf("|    |      |      |     |\n");
     printf("|____|______|______|_____|\n");
-    printf("|%d  | %d   | %d   | %d  |\n", a[3][0], a[3][1], a[3][2], a[3][3]);
+    printf("|%s  | %s   | %s   | %s  |\n", a[3][0], a[3][1], a[3][2], a[3][3]);
     printf("|    |      |      |     |\n");
     printf("|____|______|______|_____|\n");
 }
