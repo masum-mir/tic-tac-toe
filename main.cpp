7871138a186d94e7f2c1103602c7262abcbb9df9
masum <masummir.lm@gmail.com>
2023-12-13 04:45:31 +0600

    Merge fixed

--- main.cpp
+++ main.cpp
@@ -12,22 +12,30 @@ void currentTime();  // current time display
 void homePage(int x, int y); // first home page
 int handleClick(int x, int y);  // board click cursor
 int controlPanalsXYcordinate(int x, int y);     // control click cursor
-void controlPanals();   // control panals
+int controlPanals();   // control panals
 void inputPlayerName(char firstPlayer[], char secondPlayer[]);    // player input
 void mouseEvent();
+int mainMenuXYcordinate(int x, int y);
+void mainSystem(int x, int y, int i, int j);
+void boardSystem(int x, int y, char *a[ROW][COL], char firstPlayer[], char secondPlayer[], int i, int j);
+int gameDraw(char *a[ROW][COL]) {
 
-int xy;
+    for(int i=0; i<ROW; i++) {
+        for(int j=0; j<COL; j++) {
+            if(*a[i][j] != ' ') {
+                     //&& *a[i][j] != "X" && *a[i][j] != "O"
+                return 1;
+            }
+        }
+    }
+return -1;
+}
+
+int currentPlayer = 1;
 
 int main()
 {
     int gd = DETECT, gm, x = 210, y = 150, i=0, j=0;
-    char firstPlayer[20], secondPlayer[20];
-    //char* firstPlayer = (char)*malloc(10);
-    //char* secondPlayer = (char)*malloc(10);
-    int currentPlayer = 1, mouseX, mouseY;
-
-    char *a[ROW][COL] = {" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "};
-
 
     initgraph(&gd, &gm, "");
 
@@ -35,34 +43,31 @@ int main()
 
     while(getch() != 13);
 
-    controlPanals();
+    mainSystem(x,y,i,j);
+    currentTime();
+
+    getch();
+    closegraph();
+    return 0;
+}
+
+void mainSystem(int x, int y, int i, int j)
+{
+
+    char firstPlayer[20]="\0", secondPlayer[20];
+    printf("len::: %d \n", strlen(firstPlayer));
+    //char* firstPlayer = (char)*malloc(10);
+    //char* secondPlayer = (char)*malloc(10);
+    //int currentPlayer = 1, mouseX, mouseY;
+
+    char *a[ROW][COL] = {" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "};
+
+
 
     while(1)
     {
 
-        int mouseClick = 0;
-        int mouseXX, mouseYY;
-        while (!mouseClick)
-        {
-            if (ismouseclick(WM_LBUTTONDOWN))
-            {
-                getmouseclick(WM_LBUTTONDOWN, mouseXX, mouseYY);
-                mouseClick = 1;
-                printf("mouse click: %d \n", mouseClick);
-            }
-        }
-
-        // dynamic mouse cursor
-        /*
-                int* mousePtr;
-                mousePtr = mouseclickEvent();
-                printf("\n\nXX : %d, YY : %d\n\n", (int)mousePtr[1], (int)mousePtr[2]);
-                free(mousePtr);
-        */
-
-        int r = controlPanalsXYcordinate(mouseXX, mouseYY);
-        printf("\n Option selected first :: %d \n\n", r);
-
+        int r = controlPanals();
         // if(kbhit())
         if(r!=-1)
         {
@@ -75,85 +80,16 @@ int main()
                 printf("\nR IS ::: %d\n", r);
                 inputPlayerName(firstPlayer, secondPlayer);
 
-                //while(!kbhit())
-                while(1)
-                {
-                    // board show
-                    cleardevice();
-                    board(x, y, a, firstPlayer, secondPlayer);
-
-                    // mouse click event
-                    int mouseClick = 0;
-                    int mouseX, mouseY;
-                    while(!mouseClick)
-                    {
-                        if(ismouseclick(WM_LBUTTONDOWN))
-                        {
-                            getmouseclick(WM_LBUTTONDOWN, mouseX, mouseY);
-                            mouseClick = 1;
-                        }
-                    }
-
-                    // board handle in mouse click
-                    int result = handleClick(mouseX, mouseY);
-                    //printf("Result :: %d ", result);
-
-                    if(result != -1)
-                    {
-                        int row = result / ROW;  // row position
-                        int col = result % COL;  // col position
-                        //printf("\n\nRow :: %d Col:: %d\n\n", row,col);
-                        //   char ch2 = (result == 1) ? "X" : "O";
-                        //printf("\n\n %c \n\n", a[row][col]);
-
-                        if(a[row][col] == " ")
-                        {
-                            // char *c = (currentPlayer==1) ? 'X' : 'O';
-                            // char *p = &c;
-                            //  a[row][col] = (currentPlayer==1) ? 'X' : 'O';
-
-                            //printf("\n\nCUrrent player :: X OR O\n\n");
-                            if(currentPlayer == 1)
-                            {
-                                //printf("\n\nCurrent player :: X\n\n");
-                                a[row][col] = "X";
-                                //circle(mouseX, mouseY, 10);
-                                //printf("\n\n %c \n\n", a[row][col]);
-                                //currentPlayer = 2;
-                            }
-                            else
-                            {
-                                //printf("\n\nCurrent player :: O\n\n");
-                                a[row][col] = "O";
-                                // currentPlayer = 1;
-                            }
-
-
-                            int gameReturn = gameWin(a, currentPlayer);
-                            printf("GAME RESURN ::: %d ", gameReturn);
-                            if( gameReturn == 1)
-                            {
-                                printf("Player %c win", (currentPlayer == 1)? 'X' : 'O');
-                                outtextxy(300, 450, "Player One Win!");
-                                board(x, y, a, firstPlayer, secondPlayer);
-                                break;
-                            }
-                            currentPlayer = 3-currentPlayer;        // current player first 1 then 2
-                        }
-
-                    }
-
-                    clearmouseclick(WM_LBUTTONDOWN);                // mouse clear event
-
-                }
+                boardSystem(x, y, a, firstPlayer, secondPlayer, i, j);
 
             }
             // else if(keyb == 99 || keyb == 68) // controls
             else if(r==2)
             {
+                printf("Main System 2");
                 cleardevice();
-                outtextxy(200, 100, "How to Play.");
-                if(kbhit())
+                outtextxy(200, 100, "How to Play Game!");
+               /* if(kbhit())
                 {
                     char ctr = getch();
                     if(ctr == 98)
@@ -161,6 +97,7 @@ int main()
                         controlPanals();
                     }
                 }
+                */
             }
             //else if(keyb == 27)  // exit
             else
@@ -170,11 +107,131 @@ int main()
         }
         clearmouseclick(WM_LBUTTONDOWN);
     }
+}
+
+//main board
+void boardSystem(int x, int y, char *a[ROW][COL], char firstPlayer[], char secondPlayer[], int i, int j)
+{
+//while(!kbhit())
+    while(1)
+    {
+        // board show
+        cleardevice();
+        board(x, y, a, firstPlayer, secondPlayer);
+
+        // mouse click event
+        int mouseClick = 0;
+        int mouseX, mouseY;
+        while(!mouseClick)
+        {
+            if(ismouseclick(WM_LBUTTONDOWN))
+            {
+                getmouseclick(WM_LBUTTONDOWN, mouseX, mouseY);
+                mouseClick = 1;
+            }
+        }
+
+        // board handle in mouse click
+        int result = handleClick(mouseX, mouseY);
+        //printf("Result :: %d ", result);
+
+        if(result != -1)
+        {
+            int row = result / ROW;  // row position
+            int col = result % COL;  // col position
+            //printf("\n\nRow :: %d Col:: %d\n\n", row,col);
+
+            if(a[row][col] == " ")
+            {
 
 
-    getch();
-    closegraph();
-    return 0;
+                if(currentPlayer == 1)
+                {
+                    //printf("\n\nCurrent player :: X\n\n");
+                    a[row][col] = "X";
+                    //circle(mouseX, mouseY, 10);
+                    //currentPlayer = 2;
+                }
+                else
+                {
+                    //printf("\n\nCurrent player :: O\n\n");
+                    a[row][col] = "O";
+                    // currentPlayer = 1;
+                }
+
+                int gameReturn = gameWin(a, currentPlayer);
+                printf("GAME RESURN ::: %d ", gameReturn);
+
+                int gameD = gameDraw(a);
+
+                printf("\n\nGame Draw:: %d\n\n", gameD);
+
+                if( gameReturn == 1)
+                {
+                    cleardevice();
+                    char str[50];
+                    sprintf(str,"Player %s Win!", (currentPlayer == 1)? "First" : "Second");
+                    outtextxy(200, 200, str);
+
+                    // again start
+                    outtextxy(250, 240, "Try again");
+                    outtextxy(240, 270, "Main Menu");
+                    outtextxy(275, 300, "Exits");
+                    while(1)
+                    {
+                        int menuClick = 0;
+                        int menuXX, menuYY;
+                        while (!menuClick)
+                        {
+                            if (ismouseclick(WM_LBUTTONDOWN))
+                            {
+                                getmouseclick(WM_LBUTTONDOWN, menuXX, menuYY);
+                                menuClick = 1;
+                                printf("Menu click: %d \n", menuClick);
+                            }
+                        }
+
+                        int r = mainMenuXYcordinate(menuXX, menuYY);
+                        printf("Mouse pointer home:: %d ", r);
+                        if(r!=-1)
+                        {
+                            if(r==1)        // not working properly
+                            {
+                                int z, zz;
+                                printf("Mouse pointer home 1 :: %d ", r);
+                                if(z>0&& zz>0)
+                                {
+                                    z--;
+                                    zz--;
+                                    *a[z][zz]='\0';
+                                }
+
+                                boardSystem(x, y, a, firstPlayer, secondPlayer, i, j);
+                            }
+                            else if(r==2)
+                            {
+                                printf("Mouse pointer home 2 :: %d ", r);
+                                mainSystem(x,y,i,j);
+                            }
+                            else
+                            {
+                                exit(0);
+                            }
+                        }
+                        clearmouseclick(WM_LBUTTONDOWN);
+                    }
+
+                    //board(x, y, a, firstPlayer, secondPlayer);
+                    break;
+                }
+                currentPlayer = 3-currentPlayer;        // current player first 1 then 2
+            }
+
+        }
+
+        //  clearmouseclick(WM_LBUTTONDOWN);                // mouse clear event
+
+    }
 }
 
 // home page
@@ -200,6 +257,7 @@ void homePage(int x, int y)
 // board draw func
 void board(int x, int y, char *board[ROW][COL], char nameF[], char nameS[])
 {
+
     settextstyle(EUROPEAN_FONT, HORIZ_DIR, 1);
     setcolor(LIGHTMAGENTA);
     outtextxy(280, 30, "TIC TAC TOE");
@@ -223,6 +281,7 @@ void board(int x, int y, char *board[ROW][COL], char nameF[], char nameS[])
         x = 210;
         y = y + 70;
     }
+  //  rectangle(150, 30, 200, 150);
     //currentTime();
 }
 
@@ -248,6 +307,7 @@ void currentTime()
         sprintf(timestr,"%02d:%02d:%02d",localTime->tm_hour,localTime->tm_min, localTime->tm_sec);
         outtextxy(500, 50,timestr);
         delay(1000);
+
     }
 
 }
@@ -291,7 +351,7 @@ int gameWin(char *arr[ROW][COL], int player)
 }
 
 // 2nd page control panal func
-void controlPanals()
+int controlPanals()
 {
     settextstyle(EUROPEAN_FONT,HORIZ_DIR, 1);
     setcolor(WHITE);
@@ -299,49 +359,44 @@ void controlPanals()
     outtextxy(220, 140, "Start Game");
     outtextxy(220, 170, "Control");
     outtextxy(220, 200, "Exits");
-    /*
-        while(1)
+
+    int mouseClick = 0;
+    int mouseXX, mouseYY;
+    while (!mouseClick)
+    {
+        if (ismouseclick(WM_LBUTTONDOWN))
         {
-
-            int mouseClick = 0;
-            int mouseXX, mouseYY;
-            while(!mouseClick)
-            {
-                if(ismouseclick(WM_LBUTTONDOWN))
-                {
-                    getmouseclick(WM_LBUTTONDOWN, mouseXX, mouseYY);
-                    mouseClick = 1;
-                    printf("mouse click: %d \n", mouseClick);
-                    //    circle(mouseX, mouseY, 10);
-
-                }
-            }
-
-            int r = controlPanalsXYcordinate(mouseXX, mouseYY);
-            printf("\n Option first :: %d \n\n", r);
-            if(r!= - 1)
-            {
-                if(r==1)
-                {
-                    xy = 13;
-                }
-                else if(r==2)
-                {
-                    xy = 66;
-                    outtextxy(220, 120, "Control");
-                }
-                else
-                {
-                    xy = 33;
-                    outtextxy(220, 140, "Exits");
-                }
-            }
-
-            clearmouseclick(WM_LBUTTONDOWN);
-
+            getmouseclick(WM_LBUTTONDOWN, mouseXX, mouseYY);
+            mouseClick = 1;
+            printf("mouse click: %d \n", mouseClick);
         }
-        */
+    }
+
+    // dynamic mouse cursor
+    /*
+            int* mousePtr;
+            mousePtr = mouseclickEvent();
+            printf("\n\nXX : %d, YY : %d\n\n", (int)mousePtr[1], (int)mousePtr[2]);
+            free(mousePtr);
+    */
+
+
+    int r = controlPanalsXYcordinate(mouseXX, mouseYY);
+    printf("\n Option selected first :: %d \n\n", r);
+    return r;
+
 }
+/*
+char* playerName(int i, int ascii)
+{
+    int *ptr[10];
+
+    ptr[i++] = ascii;
+    firstPlayer[i]=0;
+
+    return
+}
+*/
 
 // input player name    func
 void inputPlayerName(char firstPlayer[], char secondPlayer[])
@@ -361,15 +416,21 @@ void inputPlayerName(char firstPlayer[], char secondPlayer[])
         {
             char ascii = getch();
             printf("Player One: %d\n", ascii);
-            if(strlen(firstPlayer) == 0)
+            printf("\nlength:: :%d", strlen(firstPlayer));
+            /*
+             if(strlen(firstPlayer) == 1)
+             {
+                 printf("Please input first name!");
+                 outtextxy(120, 100, "Please input first name!");
+                 firstPlayer[i++] = ascii;
+                 firstPlayer[i]=0;
+                 outtextxy(350, 150, firstPlayer);
+                 //continue;
+             }
+            */
+            if(ascii == 13)
             {
-                printf("Please input first name!");
-                outtextxy(120, 100, "Please input first name!");
-                continue;
-            }
 
-            if(ascii == 13 && strlen(firstPlayer) != 0)
-            {
                 break;
             }
             else if(ascii == 8)
@@ -378,6 +439,9 @@ void inputPlayerName(char firstPlayer[], char secondPlayer[])
                 {
                     i--;
                     firstPlayer[i] = '\0';
+
+                    //memset(firstPlayer, '\0', sizeof(firstPlayer));
+                    printf("\n\nLength:: %d\n\n", strlen(firstPlayer));
                     cleardevice();
                     outtextxy(350, 150, firstPlayer);
                     outtextxy(240, 230, "Enter");
@@ -388,12 +452,12 @@ void inputPlayerName(char firstPlayer[], char secondPlayer[])
             else
             {
                 //  first player name
+                //  firstPlayer = playerName(i, ascii);
                 firstPlayer[i++] = ascii;
                 firstPlayer[i]=0;
                 outtextxy(350, 150, firstPlayer);
             }
         }
-
     }
 
     while(1)
@@ -431,6 +495,24 @@ void inputPlayerName(char firstPlayer[], char secondPlayer[])
     }
 }
 
+int mainMenuXYcordinate(int x, int y)
+{
+    printf("\n\nFirst row : %d , col : %d\n\n", x,y);
+
+    if((x>=245 && x<=353) && (y>=237 && y<=260))
+    {
+        return 1;
+    }
+    if((x>=238 && x<=360) && (y>=269 && y<=286))
+    {
+        return 2;
+    }
+    if((x>=270 && x<=330) && (y>=300 && y<=320))
+    {
+        return 3;
+    }
+    return -1;
+}
 
 int controlPanalsXYcordinate(int x, int y)
 {
@@ -441,9 +523,8 @@ int controlPanalsXYcordinate(int x, int y)
     {
         return 1;
     }
-    if((x>=220 && x<=266) && (y>=172 && y<=185))
+    if((x>=220 && x<=300) && (y>=172 && y<=185))
     {
-
         return 2;
     }
     if((x>=220 && x<=255) && (y>=203 && y<=215))
@@ -461,25 +542,73 @@ int handleClick(int x, int y)
 {
     printf("\n\nX ::: %d \n\n, Y ::: %d \n", x, y);
 
-    if ( (x > 210 && x <= 280) && (y > 150 && y<=220)) return 0;
-    if ( (x > 280 && x <= 350) && (y > 150 && y<=220)) return 1;
-    if ( (x > 350 && x <= 420) && (y > 150 && y<=220)) return 2;
-    if ( (x > 420 && x <= 490) && (y > 150 && y<=220)) return 3;
+    if ( (x > 210 && x <= 280) && (y > 150 && y<=220))
+    {
+        return 0;
+    }
+    if ( (x > 280 && x <= 350) && (y > 150 && y<=220))
+    {
+        return 1;
+    }
+    if ( (x > 350 && x <= 420) && (y > 150 && y<=220))
+    {
+        return 2;
+    }
+    if ( (x > 420 && x <= 490) && (y > 150 && y<=220))
+    {
+        return 3;
+    }
 
-    if ( (x > 210 && x <= 280) && (y > 220 && y<=290)) return 4;
-    if ( (x > 280 && x <= 350) && (y > 220 && y<=290)) return 5;
-    if ( (x > 350 && x <= 420) && (y > 220 && y<=290)) return 6;
-    if ( (x > 420 && x <= 490) && (y > 220 && y<=290)) return 7;
+    if ( (x > 210 && x <= 280) && (y > 220 && y<=290))
+    {
+        return 4;
+    }
+    if ( (x > 280 && x <= 350) && (y > 220 && y<=290))
+    {
+        return 5;
+    }
+    if ( (x > 350 && x <= 420) && (y > 220 && y<=290))
+    {
+        return 6;
+    }
+    if ( (x > 420 && x <= 490) && (y > 220 && y<=290))
+    {
+        return 7;
+    }
 
-    if ( (x > 210 && x <= 280) && (y > 290 && y<=360)) return 8;
-    if ( (x > 280 && x <= 350) && (y > 290 && y<=360)) return 9;
-    if ( (x > 350 && x <= 420) && (y > 290 && y<=360)) return 10;
-    if ( (x > 420 && x <= 490) && (y > 290 && y<=360)) return 11;
+    if ( (x > 210 && x <= 280) && (y > 290 && y<=360))
+    {
+        return 8;
+    }
+    if ( (x > 280 && x <= 350) && (y > 290 && y<=360))
+    {
+        return 9;
+    }
+    if ( (x > 350 && x <= 420) && (y > 290 && y<=360))
+    {
+        return 10;
+    }
+    if ( (x > 420 && x <= 490) && (y > 290 && y<=360))
+    {
+        return 11;
+    }
 
-    if ( (x > 210 && x <= 280) && (y > 360 && y<=430)) return 12;
-    if ( (x > 280 && x <= 350) && (y > 360 && y<=430)) return 13;
-    if ( (x > 350 && x <= 420) && (y > 360 && y<=430)) return 14;
-    if ( (x > 420 && x <= 490) && (y > 360 && y<=430)) return 15;
+    if ( (x > 210 && x <= 280) && (y > 360 && y<=430))
+    {
+        return 12;
+    }
+    if ( (x > 280 && x <= 350) && (y > 360 && y<=430))
+    {
+        return 13;
+    }
+    if ( (x > 350 && x <= 420) && (y > 360 && y<=430))
+    {
+        return 14;
+    }
+    if ( (x > 420 && x <= 490) && (y > 360 && y<=430))
+    {
+        return 15;
+    }
 
     return -1;
 }
